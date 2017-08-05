0f5a57ac75aa8a2d7483766134b5a95b3c3a9fa4
榮長幹雄 <0419mikio@gmail.com>
2017-08-05 20:54:56 +0900

    07
    8月5日　20:55

--- Assets/G1.cs
+++ Assets/G1.cs
@@ -1,57 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class G1 : MonoBehaviour {
 
-	private float upForce = 0.6f;
 
 	private Rigidbody myRigidbody;
 
-	private bool isupButtonDown = false;
-
-	private bool isdownButtonDown = false;
 
 	// Use this for initialization
 	void Start () {
 
 		myRigidbody = GetComponent<Rigidbody>();
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
 
-		if (this.isupButtonDown)
-		{
-			this.myRigidbody.AddForce(0, this.upForce, 0);
-		}
-		else if (this.isdownButtonDown)
-		{
-			this.myRigidbody.AddForce(0, -this.upForce, 0);
-		}
-
-	}
-
-	//左ボタンを押し続けた場合の処理（追加）
-	public void GetdownButtonDown()
-	{
-		this.isdownButtonDown = true;
-	}
-	//左ボタンを離した場合の処理（追加）
-	public void GetdownButtonUp()
-	{
-		this.isdownButtonDown = false;
-	}
-
-	//右ボタンを押し続けた場合の処理（追加）
-	public void GetupButtonDown()
-	{
-		this.isupButtonDown = true;
-	}
-	//右ボタンを離した場合の処理（追加）
-	public void GetupButtonUp()
-	{
-		this.isupButtonDown = false;
 	}
 }
--- Assets/Item.cs
+++ Assets/Item.cs
@@ -16,29 +16,28 @@ public class Item : MonoBehaviour
 	private int startPos = -310;
 	//ゴール地点
 	private int goalPos = 340;
-	//アイテムを出すx方向の範囲
+	//アイテムを出すy方向の範囲
 	private float posRange = 40.0f;
+
+	private float upForce = 0.6f;
+
+
+	private List<GameObject> barriers;
+
+	bool isButtonDown;
+
+	bool isLButtonDown;
+
 
 	// Use this for initialization
 	void Start()
 	{
+		barriers = new List<GameObject>();
 
 		//一定の距離ごとにアイテムを生成
 		for (int i = startPos; i < goalPos; i += 30)
 		{
 			if (i > -200)
-			{
-				i -= 5;
-			}
-			else if (-200 < i && i < -50)
-			{
-				i -= 5;
-			}
-			else if (-50 < i && i < 200)
-			{
-				i = 5;
-			}
-			else if (200 < i)
 			{
 				i -= 5;
 			}
@@ -57,7 +56,9 @@ public class Item : MonoBehaviour
 				{
 					GameObject G1 = Instantiate(G1Pre) as GameObject;
 					G1.transform.position = new Vector3(G1.transform.position.x, posRange * j, i + offsetZ);
+					barriers.Add(G1);
 				}
+
 			}
 			for (float j = -0.35f; j < 0.35; j += 0.06f)
 			{
@@ -72,7 +73,9 @@ public class Item : MonoBehaviour
 					//G2を生成
 					GameObject G2 = Instantiate(G2Pre) as GameObject;
 					G2.transform.position = new Vector3(G2.transform.position.x, posRange * j, i + offsetZ);
+					barriers.Add(G2);
 				}
+
 			}
 			for (float j = -0.35f; j < 0.35; j += 0.08f)
 			{
@@ -87,13 +90,56 @@ public class Item : MonoBehaviour
 					//G3を生成
 					GameObject G3 = Instantiate(G3Pre) as GameObject;
 					G3.transform.position = new Vector3(G3.transform.position.x, posRange * j, i + offsetZ);
+					barriers.Add(G3);
 				}
+
 			}
 		}
-	}
-	// Update is called once per frame
-	void Update()
-	{
 
+	}
+
+
+	public void OnButtonDown()
+	{
+		Debug.Log("a");
+		isButtonDown = true;
+	}
+
+	public void OnButtonUp()
+	{
+		Debug.Log("b");
+		isButtonDown = false;
+	}
+
+
+	public void OnLButtonDown()
+	{
+		Debug.Log("c");
+		isLButtonDown = true;
+	}
+
+	public void OnLButtonUp()
+	{
+		Debug.Log("d");
+		isLButtonDown = false;
+	}
+
+	public void Update()
+	{
+		if (isButtonDown)
+		{
+			foreach (var barrier in barriers)
+			{
+				barrier.GetComponent<Rigidbody>().AddForce(0, this.upForce, 0);
+			}
+		}
+
+		if (isLButtonDown)
+		{
+			foreach (var barrier in barriers)
+			{
+				barrier.GetComponent<Rigidbody>().AddForce(0, -this.upForce, 0);
+			}
+		}
 	}
 }
