385e8bc9e198f478568c1a85a35517522d3a8e9a
榮長幹雄 <0419mikio@gmail.com>
2017-08-05 23:18:58 +0900

    08
    8月5日　23:18

---
+++ Assets/line.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class line : MonoBehaviour {
+
+	private GameObject ball;
+	private float L;
+	private float startPos = a;
+	private float goalPos = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+		this.ball= GameObject.Find("ball");
+		this.L = goalPos - ball.transform.position.z/683;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.localScale = new Vector3(L, 1, 1);
+	}
+}
